01d7bb550b9e686f68fb49aa0e6d9a2892c96ef7
daeyoung-k <eodud4976@gmail.com>
2023-09-27 17:31:14 +0900

    api domain

---
+++ src/main/kotlin/com/fastcampus/fcboard/controller/PostController.kt
@@ -0,0 +1,59 @@
+package com.fastcampus.fcboard.controller
+
+import com.fastcampus.fcboard.controller.dto.*
+import org.springframework.data.domain.Page
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.awt.print.Pageable
+import java.time.LocalDateTime
+
+@RestController
+class PostController {
+
+    @PostMapping("/posts")
+    fun createPost(
+        @RequestBody postCreateRequest: PostCreateRequest,
+    ): Long{
+        return 1
+    }
+
+    @PutMapping("/posts/{id}")
+    fun updatePost(
+        @PathVariable id: Long,
+        @RequestBody postUpdateRequest: PostUpdateRequest,
+    ): Long{
+        return id
+    }
+
+    @DeleteMapping("/posts/{id}")
+    fun deletePost(
+        @PathVariable id: Long,
+        @RequestParam createdBy: String
+    ): Long{
+        println(createdBy)
+        return id
+    }
+
+    @GetMapping("/posts/{id}")
+    fun getPost(
+        @PathVariable id: Long
+    ): PostDetailResponse {
+        return PostDetailResponse(1, "title", "content", "kane", LocalDateTime.now().toString())
+    }
+
+    @GetMapping("/posts")
+    fun getPosts(
+        pageable: Pageable,
+        postSearchRequest: PostSearchRequest
+    ): Page<PostSummaryResponse> {
+        println("title: ${postSearchRequest.title}")
+        println("createdBy: ${postSearchRequest.createdBy}")
+        return Page.empty()
+    }
+}
